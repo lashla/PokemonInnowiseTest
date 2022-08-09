b9daa799f7006b31857b64619171b0b4d6cc7ae3
lashla <lash.la412@gmail.com>
2022-08-09 17:13:25 +0300

    Created data classes to fetch Api's data and data class for local database to store items efficiently

---
+++ app/src/main/java/com/lasha/pokemoninnowisetest/data/entities/DbEntity.kt
@@ -0,0 +1,15 @@
+package com.lasha.pokemoninnowisetest.data.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "pokemons")
+data class DbEntity(
+    @PrimaryKey @ColumnInfo(name = "id") var id: Int,
+    @ColumnInfo(name = "name") var name: String,
+    @ColumnInfo(name = "weight") var weight: Int?,
+    @ColumnInfo(name = "height") var height: Int?,
+    @ColumnInfo(name = "type") var type: String?,
+    @ColumnInfo(name = "image") var image: String?
+)
---
+++ app/src/main/java/com/lasha/pokemoninnowisetest/data/entities/pokemon.kt
@@ -0,0 +1,25 @@
+package com.lasha.pokemoninnowisetest.data.entities
+
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+
+data class Pokemon(
+    @PrimaryKey
+    val id: Int,
+    val name: String,
+    val height: Int,
+    val weight: Int,
+    val types: List<PokemonType>,
+    val sprites: PokemonSprites
+)
+
+data class PokemonSprites(
+    @SerializedName("front_default")
+    val frontDefault: String?,
+)
+
+data class PokemonType(
+    val slot: Int,
+    val type: NamedApiResource
+)
